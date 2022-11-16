fc013c183cd25847166082ccd001fa168ee7a008
iLBigga <Giannanti1996@gmail.com>
2022-11-16 19:19:43 +0100

    Add sponsor seeder, migration

---
+++ database/seeds/SponsorSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Sponsor;
+use Illuminate\Database\Seeder;
+
+class SponsorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+        $plans = config('sponsor');
+
+        foreach($plans as $plane) {
+            $p = new Sponsor();
+            $p->plane = $plane['plane'];
+            $p->price = $plane['price'];
+            $p->duration = $plane['duration'];
+
+            $p->save();
+        }
+
+    }
+}
