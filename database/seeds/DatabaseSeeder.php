18b0a195e55884e9b1fceba2fcaf7a08dec8f28b
spellcry <sangiothebest@hotmail.com>
2022-11-17 10:29:34 +0100

    add user migration & seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+                UserSeeder::class,
                 ApartmentSeeder::class,
                 ServiceSeeder::class,
                 SponsorSeeder::class
