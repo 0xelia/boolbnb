a1eb044971776aa0642360a654d3e15e0d761927
Leonomics <leocv97@gmail.com>
2022-11-23 09:43:48 +0100

    add front end validation (required)

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -9,8 +9,8 @@
             <form action="{{ route('admin.apartments.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="title">Titolo:</label>
-                    <input class="p-2 flex-grow" type="text" name="title" id="title" placeholder="Inserisci un titolo" value="{{ old('title') }}">
+                    <label class="mr-2 font-bold" for="title">Titolo *</label>
+                    <input class="p-2 flex-grow" type="text" name="title" id="title" placeholder="Inserisci un titolo" value="{{ old('title') }}" required>
                     @error('title')
                         <p class="text-red-700">
                             {{$message}}
@@ -18,8 +18,8 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="rooms">Numero camere:</label>
-                    <input class="p-2 flex-grow" type="number" name="rooms_number" id="rooms" placeholder="Inserisci numero delle camere" value="{{ old('rooms_number') }}">
+                    <label class="mr-2 font-bold" for="rooms">Numero camere *</label>
+                    <input class="p-2 flex-grow" type="number" name="rooms_number" id="rooms" placeholder="Inserisci numero delle camere" value="{{ old('rooms_number') }}" required>
                     @error('rooms_number')
                         <p class="text-red-700">
                             {{$message}}
@@ -27,8 +27,8 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="beds">Numero letti:</label>
-                    <input class="p-2 flex-grow" type="number" name="beds_number" id="beds" placeholder="Inserisci numero dei letti" value="{{ old('beds_number') }}">
+                    <label class="mr-2 font-bold" for="beds">Numero letti *</label>
+                    <input class="p-2 flex-grow" type="number" name="beds_number" id="beds" placeholder="Inserisci numero dei letti" value="{{ old('beds_number') }}" required>
                     @error('beds_number')
                         <p class="text-red-700">
                             {{$message}}
@@ -36,8 +36,8 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="baths">Numero bagni:</label>
-                    <input class="p-2 flex-grow" type="number" name="bath_number" id="baths" placeholder="Inserisci numero dei bagni" value="{{ old('bath_number') }}">
+                    <label class="mr-2 font-bold" for="baths">Numero bagni *</label>
+                    <input class="p-2 flex-grow" type="number" name="bath_number" id="baths" placeholder="Inserisci numero dei bagni" value="{{ old('bath_number') }}" required>
                     @error('bath_number')
                         <p class="text-red-700">
                             {{$message}}
@@ -45,8 +45,8 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="meters">Metri quadri:</label>
-                    <input class="p-2 flex-grow" type="number" name="meters" id="meters" placeholder="Inserisci metri quadri" value="{{ old('meters') }}">
+                    <label class="mr-2 font-bold" for="meters">Metri quadri *</label>
+                    <input class="p-2 flex-grow" type="number" name="meters" id="meters" placeholder="Inserisci metri quadri" value="{{ old('meters') }}" required>
                     @error('meters')
                         <p class="text-red-700">
                             {{$message}}
@@ -82,7 +82,7 @@
                 </div>-->
 
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold">Servizi:</label>
+                    <label class="mr-2 font-bold">Servizi</label>
                     <ul>
                         @foreach ($services as $service)
                             <li>
@@ -99,8 +99,8 @@
                 </div>
 
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="price">Prezzo:</label>
-                    <input class="p-2 flex-grow" type="number" name="price" id="price" placeholder="Inserisci il prezzo" value="{{ old('price') }}">
+                    <label class="mr-2 font-bold" for="price">Prezzo *</label>
+                    <input class="p-2 flex-grow" type="number" name="price" id="price" placeholder="Inserisci il prezzo" value="{{ old('price') }}" required>
                     @error('price')
                         <p class="text-red-700">
                             {{$message}}
@@ -108,7 +108,7 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="image">Aggiungi immagine di copertina:</label>
+                    <label class="mr-2 font-bold" for="image">Aggiungi immagine di copertina</label>
                     <input type="file" name="image" id="image">
                     @error('image')
                         <p class="text-red-700">
@@ -117,7 +117,7 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold" for="images">Aggiungi immagine/i alla galleria:</label>
+                    <label class="mr-2 font-bold" for="images">Aggiungi immagine/i alla galleria</label>
                     <input type="file" name="images[]" id="images" multiple>
                     @error('images[]')
                         <p class="text-red-700">
@@ -126,9 +126,9 @@
                     @enderror
                 </div>
                 <div class="flex flex-col gap-2 mb-4">
-                    <label class="mr-2 font-bold">Visibilità:</label>
+                    <label class="mr-2 font-bold">Visibilità *</label>
                     <div>
-                        <input class="p-2" type="radio" name="visible" id="visible" value="true">
+                        <input class="p-2" type="radio" name="visible" id="visible" value="true" required>
                         <label class="mr-2" for="visible">Visibile</label>
                     </div>
                     <div>
@@ -143,6 +143,7 @@
                 </div>
                 <input class="p-2 border-2 rounded-lg bg-transparent cursor-pointer" type="submit" value="Salva">
             </form>
+            <div><strong>*</strong> indica un campo obbligatorio</div>
         </div>
     </div>
 @endsection
