3d8e4ca50acc92c0225a6e7dba9c3f148c9ae80f
0xelia <elia.vanon@yahoo.com>
2022-11-24 19:31:39 +0100

    message badge

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -13,13 +13,16 @@
         <ul class="grid grid-cols-4 gap-4">
             @foreach ($apartments as $apartment)
                 <a href="{{ route('admin.apartments.show', $apartment) }}">
-                    <li class="flex h-full flex-col">
+                    <li class="flex h-full flex-col relative">
                         <figure class="overflow-hidden flex-grow rounded-xl">
                             <img class="w-full h-full object-cover object-center" src="{{ $apartment->pic_path }}" alt="">
                         </figure>
                         <h3 class="font-bold capitalize">
                             {{ $apartment->title }}
                         </h3>
+                        <div class="absolute notification flex items-center justify-center  rounded-full h-6 w-6 text-center text-xs bg-red-500 text-white">
+                            <span> {{count($apartment->messages)}} </span>
+                        </div>
                     </li>
                 </a>
             @endforeach
