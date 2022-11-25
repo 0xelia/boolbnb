f47ff4436f79f599bb24c8dfabc8fe3d39861546
iLBigga <Giannanti1996@gmail.com>
2022-11-25 17:43:58 +0100

    Add header + tailwind conf

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <Header/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from '../components/HeaderComponent.vue';
+
 export default {
+  components: {
+    Header,
+  }
 
 }
 </script>
