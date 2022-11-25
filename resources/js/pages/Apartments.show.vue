6bfbec56a026797a112db95d363c5be7c05ee9f8
MaryChiodelli <marychiodelli97@gmail.com>
2022-11-25 12:35:02 +0100

    add front office setup

---
+++ resources/js/pages/Apartments.show.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Apartment show</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!--tom tom -->
+    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.5.0/maps/maps.css'>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.5.0/maps/maps-web.min.js"></script>
+
+</head>
+<body class="bg-gray-100 h-screen antialiased leading-none">
+    <div id="app"></div>
+
+     <!-- Scripts -->
+     <script src="{{ asset('js/front.js') }}" defer></script>
+</body>
+</html>
