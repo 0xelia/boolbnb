62c2c1e65c430c67f534249cd15c3279f6ec890b
MaryChiodelli <marychiodelli97@gmail.com>
2022-11-26 13:47:22 +0100

    add form fields

---
+++ resources/js/components/AccordionFilter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="py-6">
+        <div class="flex justify-between gap-4">
+            <h3>{{ info.title }}</h3>
+            <i class="fa-solid fa-plus"></i>
+        </div>
+        <ul class="mt-6 flex flex-col gap-4">
+            <li v-for="(field, i) in fields" :key="i" class="flex gap-3">
+                <input :type="info.type" :name="info.name" :id="field.id">
+				<label :for="field.id">{{ field.label }}</label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        info: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            fields: this.info.filters
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/pages/AdvancedSearch.vue
+++ resources/js/pages/AdvancedSearch.vue
@@ -5,7 +5,9 @@
 			<SearchBox class="mx-auto" />
 		</div>
 		<div class="container flex flex-col lg:flex-row lg:gap-8">
-			<div class="lg:w-1/4">filtri</div>
+			<div class="lg:w-1/4">
+				<AccordionFilter v-for="(category, i) in categories" :key="i" :info="category" />
+			</div>
 			<div class="lg:flex-grow">cards</div>
 		</div>
 	</main>
@@ -13,10 +15,113 @@
 
 <script>
 import SearchBox from '../components/SearchBox.vue'
+import AccordionFilter from '../components/AccordionFilter.vue'
 
 export default {
+	data() {
+		return {
+			categories: [
+				{
+					title: 'Stanze',
+					type: 'radio',
+					name: 'rooms',
+					filters: [
+						{
+							label: '1',
+							id: 'room-1'
+						},
+						{
+							label: '2',
+							id: 'room-2'
+						},
+						{
+							label: '3',
+							id: 'room-3'
+						},
+					],
+				},
+				{
+					title: 'Letti',
+					type: 'radio',
+					name: 'beds',
+					filters: [
+						{
+							label: '1',
+							id: 'bed-1'
+						},
+						{
+							label: '2',
+							id: 'bed-2'
+						},
+						{
+							label: '3',
+							id: 'bed-3'
+						},
+					],
+				},
+				{
+					title: 'Bagni',
+					type: 'radio',
+					name: 'baths',
+					filters: [
+						{
+							label: '1',
+							id: 'bath-1'
+						},
+						{
+							label: '2',
+							id: 'bath-2'
+						},
+						{
+							label: '3',
+							id: 'bath-3'
+						},
+					],
+				},
+				{
+					title: 'Distanza',
+					type: 'radio',
+					name: 'meters',
+					filters: [
+						{
+							label: '5 km',
+							id: 'meter-1'
+						},
+						{
+							label: '10 km',
+							id: 'meter-2'
+						},
+						{
+							label: '20km',
+							id: 'meter-3'
+						},
+					],
+				},
+				{
+					title: 'Servizi',
+					type: 'checkbox',
+					name: 'services',
+					filters: [
+						{
+							label: 'WiFi',
+							id: 'wifi-1'
+						},
+						{
+							label: 'Piscina',
+							id: 'wifi-2'
+						},
+						{
+							label: 'Parcheggio macchina',
+							id: 'wifi-3'
+						},
+					],
+				},
+			]
+		}
+	},
 	components: {
 		SearchBox,
+		AccordionFilter,
 	}
 }
 </script>
