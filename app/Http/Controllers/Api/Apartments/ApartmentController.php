230e004fb04e6daac86e85ea5e8bd8d9cb9bd4bf
spellcry <sangiothebest@hotmail.com>
2022-11-26 04:11:30 +0100

    add curl request (not working)

--- app/Http/Controllers/Api/Apartments/ApartmentController.php
+++ app/Http/Controllers/Api/Apartments/ApartmentController.php
@@ -29,6 +29,72 @@ class ApartmentController extends Controller
                 'success' => true
             ]);
         }
+        if($type === 'advanced_search') {            
+            $key = config('tomtom');
+            $apiUrl = 'https://api.tomtom.com/search/2/geometryFilter.json';
+            $geometryList = [
+                [
+                    "position" => "40.80558,-73.96548",
+                    "radius" => 100,
+                    "type" => "CIRCLE",
+                ],
+                [
+                    "type" => "POLYGON",
+                    "vertices" => [
+                        "37.7524152343544,-122.43576049804686",
+                        "37.70660472542312,-122.43301391601562",
+                        "37.712059855877314,-122.36434936523438",
+                        "37.75350561243041,-122.37396240234374"
+                    ]
+                ]
+            ];
+            $geometryList = json_encode($geometryList);
+            $poiList = [
+                [
+                    "poi" => [
+                        "name" => "S Restaurant Toms"
+                    ],
+                    "address" => [
+                        "freeformAddress" => "2880 Broadway, New York, NY 10025"
+                    ],
+                    "position" => [
+                        "lat" => 40.80558,
+                        "lon" => -73.96548
+                    ]
+                    ],
+                [
+                    "poi" => [
+                        "name" => "Yasha Raman Corporation"
+                    ],
+                    "address" => [
+                        "freeformAddress" => "940 Amsterdam Ave, New York, NY 10025"
+                    ],
+                    "position" => [
+                        "lat" => 40.80076,
+                        "lon" => -73.96556
+                    ]
+                ]
+            ];
+            $poiList = json_encode($poiList);
+            $params = "geometryList=" . $geometryList . "&poiList=" . $poiList;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $apiUrl . "?key=" . $key);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            $response = curl_exec($ch);
+            var_dump(curl_error($ch));
+            var_dump($response);
+            curl_close($ch);
+            $data = json_decode($response, true);
+            var_dump($data);
+            return response()->json([
+                'results' => $data,
+                'success' => true
+            ]);
+        }
     }
 
     /**
--- resources/js/pages/AdvancedSearch.vue
+++ resources/js/pages/AdvancedSearch.vue
@@ -4,7 +4,17 @@
 
 <script>
 export default {
+  methods: {
+    fetchApartments() {
+      axios.get('/api/apartments/index/advanced_search')
+        .then(res => {
 
+        })
+    }
+  },
+  created() {
+    this.fetchApartments()
+  }
 }
 </script>
 
