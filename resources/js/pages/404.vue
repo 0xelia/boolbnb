06dd6fafdbe8552f17b6cb2edd181925c062a298
Leonomics <leocv97@gmail.com>
2022-12-01 11:40:43 +0100

    add 404 base text

--- resources/js/pages/404.vue
+++ resources/js/pages/404.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>404</div>
+  <div>
+    <p class="text-6xl">
+      Oops!
+    </p>
+    <p>Sembra che non riusciamo a trovare la pagina che stai cercando.</p>
+    <p>Codice di errore: 404</p>
+    <p>Di seguito alcuni link utili:</p>
+    <ul>
+      <li><a></a></li>
+      <li><a></a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
