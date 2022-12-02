a9689ca28464f3a1f6fcd9edfe54b65698945f7c
MaryChiodelli <marychiodelli97@gmail.com>
2022-12-02 13:09:21 +0100

    edit login + logout

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -6,11 +6,11 @@
                 <nav class="ml-auto">
                     <div>
                         <template v-if="user">
-                            <a href="admin/apartments" class="no-underline hover:underline">I miei appartamenti</a>
-                            <a @click="logout" href="#" class="no-underline hover:underline pr-6">Logout</a>                            
+                            <a href="admin/apartments" class="no-underline hover:underline mr-6">I miei appartamenti</a>
+                            <a @click="logout" href="#" class="no-underline hover:underline">Logout</a>                            
                         </template>
                         <template v-else>
-                            <a href="login" class="no-underline hover:underline pr-6">Login</a>
+                            <a href="login" class="no-underline hover:underline mr-6">Login</a>
                             <a href="register" class="no-underline hover:underline">Register</a>
                         </template>
                     </div>
